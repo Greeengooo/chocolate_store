49e948e488a0713ec7663c8e23b080b4cc08f42d
Alexandro <aanoshyn@gmail.com>
2021-11-05 00:52:02 +0200

    lambda_handler

--- choco.py
+++ choco.py
@@ -1,9 +1,17 @@
 import boto3
 
-if __name__ == '__main__':
-    client = boto3.client('lambda')
-    response = client.invoke(
-        FunctionName='chocolate',
-        InvocationType='RequestResponse'
-    )
-    print(response['Payload'].read())
+
+def lambda_handler(event, context):
+    ls = []
+    k = int(event["budget"])
+    res = k
+    while k != 0:
+        ls.append(k % 3)
+        k = k // 3
+        res += k
+    reminder = sum(ls) // 3
+    maximum_number = res + reminder
+    return {
+        'statusCode': 200,
+        'body': maximum_number
+    }
